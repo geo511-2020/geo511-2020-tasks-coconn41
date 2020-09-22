2a0f8ed7632d7e54978e897f8aa526f8bdff7736
Collin O'Connor <collinoconnor3@gmail.com>
2020-09-21 22:47:12 -0400

    Adding second plot. Won't work, when I add second geom point it returns that object country isn't found

--- week_03/case_study_03.R
+++ week_03/case_study_03.R
@@ -15,3 +15,21 @@ ggplot(GapMKuwait,aes(lifeExp,gdpPercap))+
   scale_size_continuous("Population (100k)") +
   scale_color_discrete("Continent") +
   theme_bw()
+
+#grouping
+gapminder_continent = GapMKuwait %>%
+  group_by(continent,year) %>%
+  summarize(gdpPercapweighted = weighted.mean(
+    x = gdpPercap, w= pop),
+    pop = sum(as.numeric(pop)))
+
+#plot # 2
+ggplot(GapMKuwait, aes(x=year,y=gdpPercap,
+  color=continent,group=country)) +
+  geom_point() +
+  geom_line() +
+  geom_point(data=gapminder_continent,aes(x=year,y=gdpPercapweighted)) +
+  facet_wrap(~continent) +
+  theme_bw()
+#Can't get to work, after including second geom_point, it returns country not found
+  
