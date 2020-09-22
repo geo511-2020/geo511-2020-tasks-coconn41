fb21bd51e8efcb87d7ea131011164e4dc0976b22
Collin O'Connor <collinoconnor3@gmail.com>
2020-09-21 22:14:22 -0400

    Post plot #1 commit

--- week_03/case_study_03.R
+++ week_03/case_study_03.R
@@ -1,1 +1,17 @@
- 
+library(tidyverse)
+library(gapminder)
+data("gapminder")
+#remove Kuwait
+GapMKuwait= gapminder %>%
+  filter(country != "Kuwait")
+
+#plot #1
+ggplot(GapMKuwait,aes(lifeExp,gdpPercap))+
+  geom_point(aes(size=pop/100000,color=continent)) +
+  scale_y_continuous(trans = 'sqrt')+
+  facet_wrap(~year,nrow=1) +
+  xlab("Life Expectancy") +
+  ylab(" GDP per capita") +
+  scale_size_continuous("Population (100k)") +
+  scale_color_discrete("Continent") +
+  theme_bw()
