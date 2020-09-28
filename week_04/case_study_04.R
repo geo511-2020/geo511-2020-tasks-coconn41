d994eae2426e68912a082824dfdd11a83b9a1c36
Collin O'Connor <collinoconnor3@gmail.com>
2020-09-28 13:14:16 -0400

    Got name for LGA

--- week_04/case_study_04.R
+++ week_04/case_study_04.R
@@ -6,17 +6,19 @@ data(airports)
 names(airports)[1]='dest'
 
 a=left_join(airports,flights,by='dest')
+#flights from JFK
 JFKdestairs= a %>% filter(origin == "JFK") %>%  
-  distinct(origin,dest,.keep_all = T) #%>%
-  #distinct(dest,.keep_all = T)
+  distinct(origin,dest,.keep_all = T) 
+#flights from LGA
 LGAdestairs=a %>% filter(origin == "LGA") %>%
   distinct(origin,dest,.keep_all = T)
+#flights from EWR
 EWRdestairs=a %>% filter(origin=="EWR") %>%
   distinct(origin,dest,.keep_all = T)
-
+#Coordinates of NYC Airports
 nycairports= a %>% filter(dest=="JFK" | dest=="LGA" | dest=="EWR") %>%
   distinct(dest,.keep_all = T)
-
+#create function for pythagorean theorem
 pythag=function(w,x,y,z){ #such that y and z are lat long of NYC airports
 sqrt(((w-y)^2)+((x-z)^2))}
 
@@ -30,8 +32,6 @@ for (i in 1:nrow(EWRdestairs)){
     c=pythag(nycairports$lat[1],nycairports$lon[1],destlatitude,destlongitude)
     EWRdistances = rbind(EWRdistances,c)}
 }
-
-
 
 JFKdistances=NULL
 for (i in 1:nrow(JFKdestairs)){
@@ -48,10 +48,14 @@ LGAdistances=NULL
 for (i in 1:nrow(LGAdestairs)){
   destlatitude = LGAdestairs$lat[i]
   destlongitude = LGAdestairs$lon[i]
-  if(is.null(distances)==TRUE){  
+  if(is.null(LGAdestairs$distancecalc==TRUE)){  
     LGAdistances=pythag(nycairports$lat[3],nycairports$lon[3],destlatitude,destlongitude)}
   else{
     c=pythag(nycairports$lat[3],nycairports$lon[3],destlatitude,destlongitude)
     LGAdistances = rbind(LGAdistances,c)}
+  if(nrow(LGAdistances)==nrow(LGAdestairs)){
+  LGAdestairs=cbind(LGAdestairs,LGAdistances)
+  }
 }
-
+#find airport furthest away from each NYC Airport
+maxjfk=JFKdistances[JFKdistances$V1==max(JFKdistances)]
