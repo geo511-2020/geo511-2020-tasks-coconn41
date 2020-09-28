971407d7484a61e98da1ccbf951042a618815029
Collin O'Connor <collinoconnor3@gmail.com>
2020-09-28 12:56:00 -0400

    Got to work.

--- week_04/case_study_04.R
+++ week_04/case_study_04.R
@@ -1,23 +1,57 @@
 library(nycflights13)
 library(tidyverse)
-
+#load data
 data(flights)
 data(airports)
+names(airports)[1]='dest'
 
-names(flights)
-str(flights$dest)
-str(flights$origin)
-str(flights$flight)
+a=left_join(airports,flights,by='dest')
+JFKdestairs= a %>% filter(origin == "JFK") %>%  
+  distinct(origin,dest,.keep_all = T) #%>%
+  #distinct(dest,.keep_all = T)
+LGAdestairs=a %>% filter(origin == "LGA") %>%
+  distinct(origin,dest,.keep_all = T)
+EWRdestairs=a %>% filter(origin=="EWR") %>%
+  distinct(origin,dest,.keep_all = T)
 
-names(airports)
-str(airports$name)
-str(airports$faa)
+nycairports= a %>% filter(dest=="JFK" | dest=="LGA" | dest=="EWR") %>%
+  distinct(dest,.keep_all = T)
+
+pythag=function(w,x,y,z){ #such that y and z are lat long of NYC airports
+sqrt(((w-y)^2)+((x-z)^2))}
+
+EWRdistances=NULL
+for (i in 1:nrow(EWRdestairs)){
+  destlatitude = EWRdestairs$lat[i]
+  destlongitude = EWRdestairs$lon[i]
+  if(is.null(distances)==TRUE){  
+    EWRdistances=pythag(nycairports$lat[1],nycairports$lon[1],destlatitude,destlongitude)}
+  else{
+    c=pythag(nycairports$lat[1],nycairports$lon[1],destlatitude,destlongitude)
+    EWRdistances = rbind(EWRdistances,c)}
+}
 
 
-data(weather)
-str(weather$origin)
 
+JFKdistances=NULL
+for (i in 1:nrow(JFKdestairs)){
+  destlatitude = JFKdestairs$lat[i]
+  destlongitude = JFKdestairs$lon[i]
+if(is.null(distances)==TRUE){  
+  JFKdistances=pythag(nycairports$lat[2],nycairports$lon[2],destlatitude,destlongitude)}
+else{
+  c=pythag(nycairports$lat[2],nycairports$lon[2],destlatitude,destlongitude)
+JFKdistances = rbind(JFKdistances,c)}
+}
 
-data(planes)
+LGAdistances=NULL
+for (i in 1:nrow(LGAdestairs)){
+  destlatitude = LGAdestairs$lat[i]
+  destlongitude = LGAdestairs$lon[i]
+  if(is.null(distances)==TRUE){  
+    LGAdistances=pythag(nycairports$lat[3],nycairports$lon[3],destlatitude,destlongitude)}
+  else{
+    c=pythag(nycairports$lat[3],nycairports$lon[3],destlatitude,destlongitude)
+    LGAdistances = rbind(LGAdistances,c)}
+}
 
-data(airlines)
