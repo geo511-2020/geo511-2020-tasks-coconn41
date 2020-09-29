c0bf5d0aef47a6a275714cfcb42442f302813520
Collin O'Connor <collinoconnor3@gmail.com>
2020-09-28 20:53:11 -0400

    Used Haversine formula instead of pythagorean theorem

--- week_04/case_study_04.R
+++ week_04/case_study_04.R
@@ -1,5 +1,6 @@
 library(nycflights13)
 library(tidyverse)
+library(geosphere)
 #load data
 data(flights)
 data(airports)
@@ -19,18 +20,18 @@ EWRdestairs=a %>% filter(origin=="EWR") %>%
 nycairports= a %>% filter(dest=="JFK" | dest=="LGA" | dest=="EWR") %>%
   distinct(dest,.keep_all = T)
 #create function for pythagorean theorem
-pythag=function(w,x,y,z){ #such that y and z are lat long of NYC airports
-sqrt(((w-y)^2)+((x-z)^2))}
+#pythag=function(w,x,y,z){ #such that y and z are lat long of NYC airports
+#sqrt(((w-y)^2)+((x-z)^2))}
 
 distances=NULL
 for (i in 1:nrow(EWRdestairs)){
   destlatitude = EWRdestairs$lat[i]
   destlongitude = EWRdestairs$lon[i]
   if(is.null(distances)==TRUE){  
-    distances=pythag(nycairports$lat[1],nycairports$lon[1],destlatitude,destlongitude)}
+    distances=distHaversine(c(nycairports$lon[1],nycairports$lat[1]),c(destlongitude,destlatitude))}
   else{
-    c=pythag(nycairports$lat[1],nycairports$lon[1],destlatitude,destlongitude)
-    distances = rbind(distances,c)}
+    c=distHaversine(c(nycairports$lon[1],nycairports$lat[1]),c(destlongitude,destlatitude))
+      distances = rbind(distances,c)}
   if(NROW(distances)==NROW(EWRdestairs)){
     EWRdestairs=cbind(EWRdestairs,distances)
     EWRmax= EWRdestairs %>% slice(which.max(distances))
@@ -42,9 +43,9 @@ for (i in 1:nrow(JFKdestairs)){
   destlatitude = JFKdestairs$lat[i]
   destlongitude = JFKdestairs$lon[i]
 if(is.null(distances)==TRUE){  
-  distances=pythag(nycairports$lat[2],nycairports$lon[2],destlatitude,destlongitude)}
+  distances=distHaversine(c(nycairports$lon[2],nycairports$lat[2]),c(destlongitude,destlatitude))}
 else{
-  c=pythag(nycairports$lat[2],nycairports$lon[2],destlatitude,destlongitude)
+  c=distHaversine(c(nycairports$lon[2],nycairports$lat[2]),c(destlongitude,destlatitude))
 distances = rbind(distances,c)}
   if(NROW(distances)==NROW(JFKdestairs)){
      JFKdestairs=cbind(JFKdestairs,distances)
@@ -57,9 +58,9 @@ for (i in 1:nrow(LGAdestairs)){
   destlatitude = LGAdestairs$lat[i]
   destlongitude = LGAdestairs$lon[i]
   if(is.null(LGAdestairs$distancecalc==TRUE)){  
-    distances=pythag(nycairports$lat[3],nycairports$lon[3],destlatitude,destlongitude)}
+    distances=distHaversine(c(nycairports$lon[3],nycairports$lat[3]),c(destlongitude,destlatitude))}
   else{
-    c=pythag(nycairports$lat[3],nycairports$lon[3],destlatitude,destlongitude)
+    c=distHaversine(c(nycairports$lon[3],nycairports$lat[3]),c(destlongitude,destlatitude))
     distances = rbind(distances,c)}
   if(NROW(distances)==NROW(LGAdestairs)){
   LGAdestairs=cbind(LGAdestairs,distances)
@@ -70,3 +71,5 @@ for (i in 1:nrow(LGAdestairs)){
 x1=rbind(EWRmax,JFKmax)
 x2=rbind(x1,LGAmax)
 finaldf=x2[c(2,21,23,27)]
+finaldf$km_distance=finaldf$distances/1000
+finaldf$mi_distance=finaldf$km_distance*(1/1.609344)
